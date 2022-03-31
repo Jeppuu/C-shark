3f958c4cd5a11bd89aac5e3b74466defb611df14
jeppuu <jennamariakoskelo@gmail.com>
2022-03-31 10:17:44 +0300

    -

--- yodaTranslator/app.js
+++
@@ -1,6 +0,0 @@
-function toYoda() {
-//fetch('https://api.funtranslations.com/translate/yoda.json')
-fetch('https://api.funtranslations.com/translate/yoda.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
