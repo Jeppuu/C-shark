378e875a6d57eac53f305fc6705ea265f2ed61bc
jenna <jennamariakoskelo@gmail.com>
2022-04-20 08:03:54 +0000

    combobox lisätty

--- encryptDecrypt/encryptDecrypt/Form1.Designer.cs
+++ encryptDecrypt/encryptDecrypt/Form1.Designer.cs
@@ -31,18 +31,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.salasanaTbx = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.cryptattuTxb = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.cryptaaBtn = new System.Windows.Forms.Button();
             this.naytaSalasanaBtn = new System.Windows.Forms.Button();
             this.vastausLbl = new System.Windows.Forms.Label();
+            this.CryptattuCbx = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 43);
+            this.label1.Location = new System.Drawing.Point(59, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 25);
             this.label1.TabIndex = 0;
@@ -53,10 +52,11 @@
             this.salasanaTbx.BackColor = System.Drawing.Color.Black;
             this.salasanaTbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.salasanaTbx.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.salasanaTbx.Location = new System.Drawing.Point(64, 72);
+            this.salasanaTbx.Location = new System.Drawing.Point(64, 70);
             this.salasanaTbx.Name = "salasanaTbx";
             this.salasanaTbx.Size = new System.Drawing.Size(182, 23);
             this.salasanaTbx.TabIndex = 2;
+            this.salasanaTbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TyhjennaVastaus);
             // 
             // panel1
             // 
@@ -66,28 +66,10 @@
             this.panel1.Size = new System.Drawing.Size(183, 5);
             this.panel1.TabIndex = 3;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.DarkKhaki;
-            this.panel2.Location = new System.Drawing.Point(360, 95);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(183, 5);
-            this.panel2.TabIndex = 6;
-            // 
-            // cryptattuTxb
-            // 
-            this.cryptattuTxb.BackColor = System.Drawing.Color.Black;
-            this.cryptattuTxb.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.cryptattuTxb.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.cryptattuTxb.Location = new System.Drawing.Point(360, 72);
-            this.cryptattuTxb.Name = "cryptattuTxb";
-            this.cryptattuTxb.Size = new System.Drawing.Size(182, 23);
-            this.cryptattuTxb.TabIndex = 5;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(355, 43);
+            this.label3.Location = new System.Drawing.Point(355, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(122, 25);
             this.label3.TabIndex = 4;
@@ -119,7 +101,7 @@
             this.naytaSalasanaBtn.Name = "naytaSalasanaBtn";
             this.naytaSalasanaBtn.Size = new System.Drawing.Size(182, 35);
             this.naytaSalasanaBtn.TabIndex = 8;
-            this.naytaSalasanaBtn.Text = "Näytä salasana";
+            this.naytaSalasanaBtn.Text = "Decryptaa";
             this.naytaSalasanaBtn.UseVisualStyleBackColor = false;
             this.naytaSalasanaBtn.Click += new System.EventHandler(this.naytaSalasanaBtn_Click);
             // 
@@ -133,17 +115,28 @@
             this.vastausLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.vastausLbl.Visible = false;
             // 
+            // CryptattuCbx
+            // 
+            this.CryptattuCbx.BackColor = System.Drawing.Color.Black;
+            this.CryptattuCbx.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CryptattuCbx.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.CryptattuCbx.FormattingEnabled = true;
+            this.CryptattuCbx.Location = new System.Drawing.Point(360, 67);
+            this.CryptattuCbx.Name = "CryptattuCbx";
+            this.CryptattuCbx.Size = new System.Drawing.Size(183, 33);
+            this.CryptattuCbx.TabIndex = 10;
+            this.CryptattuCbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CryptattuCbx_MouseDown);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(582, 302);
+            this.Controls.Add(this.CryptattuCbx);
             this.Controls.Add(this.vastausLbl);
             this.Controls.Add(this.naytaSalasanaBtn);
             this.Controls.Add(this.cryptaaBtn);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.cryptattuTxb);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.salasanaTbx);
@@ -151,7 +144,7 @@
             this.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Encrypt & Decrypt";
             this.ResumeLayout(false);
@@ -164,12 +157,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox salasanaTbx;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox cryptattuTxb;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button cryptaaBtn;
         private System.Windows.Forms.Button naytaSalasanaBtn;
         private System.Windows.Forms.Label vastausLbl;
+        private System.Windows.Forms.ComboBox CryptattuCbx;
     }
 }
 
--- encryptDecrypt/encryptDecrypt/Form1.cs
+++ encryptDecrypt/encryptDecrypt/Form1.cs
@@ -8,13 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Eramake;
-using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;   //käytetään eramake ja mysql kirjastoja
 
 namespace encryptDecrypt
 {
     public partial class Form1 : Form
     {
-        YHDISTA yhteys = new YHDISTA();
+        YHDISTA yhteys = new YHDISTA(); //luodaan ilmentymä YHDISTÄ luokasta
 
         public Form1()
         {
@@ -24,33 +24,72 @@ namespace encryptDecrypt
         private void cryptaaBtn_Click(object sender, EventArgs e)
         {
             
-            String salattu = eCryptography.Encrypt(cryptattuTxb.Text);
+            String salattu = eCryptography.Encrypt(salasanaTbx.Text);   //kryptataan syötetty salasana käyttäen eramakea
             MySqlCommand cmd = new MySqlCommand();
-            String lisakysely = "INSERT INTO salasana(salasana) VALUES (@ssa);";
-            cmd.CommandText = lisakysely;
+            String lisakysely = "INSERT INTO salasana(salasana) VALUES (@ssa);";    //luodaan sql komento joka lisää
+            cmd.CommandText = lisakysely;                                           //salasanan tietokantaan
             cmd.Connection = yhteys.OtaYhteys();
-            cmd.Parameters.Add("@ssa", MySqlDbType.VarChar).Value = salattu;
+            cmd.Parameters.Add("@ssa", MySqlDbType.VarChar).Value = salattu;        //@ssa saa arvon salattu
 
-            yhteys.AvaaYhteys();
-            if (cmd.ExecuteNonQuery() == 1)
+            yhteys.AvaaYhteys();   //avataan yhteys, lisätään salasana ja suljetaan yhteys
+            if (cmd.ExecuteNonQuery() == 1 && salasanaTbx.Text != "")
             {
                 yhteys.SuljeYhteys();
                 vastausLbl.Text = "Salasanasi on cryptattu ja viety tietokantaan.";
                 salasanaTbx.Text = "";
                 vastausLbl.Visible = true;
             }
+            else if (salasanaTbx.Text == "")    //jos salasanaa ei syötetty, annetaan viesti
+            {
+                vastausLbl.Visible = true;
+                vastausLbl.Text = "Syötä salasana!";
+            }
             else
             {
                 yhteys.SuljeYhteys();
             }
+
+            CryptattuCbx.DataSource = HaeSalasanat(salattu);
         }
 
         private void naytaSalasanaBtn_Click(object sender, EventArgs e)
         {
-            String salasana = eCryptography.Decrypt(cryptattuTxb.Text);
-            vastausLbl.Text = "Salasanasi oli: " + salasana;
-            vastausLbl.Visible = true;
-            cryptattuTxb.Text = "";
+            String salasana = eCryptography.Decrypt(CryptattuCbx.Text); //decryptataan cryptattu salasana
+            
+            if (CryptattuCbx.SelectedIndex != 0)
+            {
+                vastausLbl.Text = "Salasanasi oli: " + salasana;            //ja näytetään se vastauskentässä
+                vastausLbl.Visible = true;                                  //jos comboboxista on valittu jotain
+                CryptattuCbx.SelectedIndex = 0;
+            }
+            else
+            {
+                vastausLbl.Text = "Valitse cryptattu salasana!";
+            }
+            
+        }
+
+        //haetaan comboboxiin cryptatut salasanat
+        public DataTable HaeSalasanat(String salasana)
+        {
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT salasana FROM salasana", yhteys.OtaYhteys());
+            DataTable dt = new DataTable();     // uusi data-adapteri hakee salasanan tietokannasta
+            da.Fill(dt);                        //ja vie tiedot datatableen
+            CryptattuCbx.DataSource = dt;
+            CryptattuCbx.DisplayMember = "salasana";    //comboboc näyttää cryptatun salasanan
+            CryptattuCbx.ValueMember = "salasana";
+            return dt;
+        }
+
+        //tyhjennetään vastaus, kun textboxia klikataan
+        private void TyhjennaVastaus(object sender, MouseEventArgs e)
+        {
+            vastausLbl.Text = "";
+        }
+        //tehdään sama comboboxille
+        private void CryptattuCbx_MouseDown(object sender, MouseEventArgs e)
+        {
+            vastausLbl.Text = "";
         }
     }
 }
